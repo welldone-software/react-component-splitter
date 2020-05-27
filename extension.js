f1cd9a4f2f13487dce34e6bb2cb58891cf9a0648
Sahar Avr <sahara@welldone-software.com>
2020-05-27 14:09:59 +0300

    used path.join to retrieve current folder path

--- extension.js
+++ extension.js
@@ -20,7 +20,7 @@ const activate = context => {
 				const selectedCode = getSelectedCode(editor);
 				await validateSelectedCode(selectedCode);
 
-				const folderPath = editor.document.uri.fsPath.replace(/[^\/]+$/, '');
+				const folderPath = path.join(editor.document.uri.fsPath, '..');
 				const subComponentName = await getSubComponentNameFromUser(folderPath);
 				const subComponentPath = path.join(folderPath, `${subComponentName}.js`);
 				const {subComponentCode, subComponentProps} = await generateSubComponentCode(editor, selectedCode, subComponentName);
